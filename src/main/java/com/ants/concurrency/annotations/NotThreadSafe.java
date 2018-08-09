13798819817207dc3f5b538ef61e4baabaca2219
iearl <dc_magaowei@163.com>
2018-08-09 16:51:15 +0800

    添加测试类

--- src/main/java/com/ants/concurrency/annotations/NotThreadSafe.java
+++ src/main/java/com/ants/concurrency/annotations/NotThreadSafe.java
@@ -1,8 +1,15 @@
 package com.ants.concurrency.annotations;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * 用来标记线程【不安全】的类或写法
  */
+@Target(ElementType.TYPE )
+@Retention(RetentionPolicy.SOURCE)
 public  @interface NotThreadSafe {
     String value() default "";
 }
--- src/main/java/com/ants/concurrency/annotations/Recomment.java
+++ src/main/java/com/ants/concurrency/annotations/Recomment.java
@@ -1,8 +1,15 @@
 package com.ants.concurrency.annotations;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * 用来标记推荐的类或写法
  */
+@Target(ElementType.TYPE )
+@Retention(RetentionPolicy.SOURCE)
 public  @interface Recomment {
     String value() default "";
 }
